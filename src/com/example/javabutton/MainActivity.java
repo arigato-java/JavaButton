ed065b4dc8048d0d148311211e98ac865f92e822
Hisanobu Tomari <htomari@myopera.com>
2013-09-28 17:24:41 +0900

    画面を擦ると早さに応じたピッチでジャバする。

--- src/com/example/javabutton/MainActivity.java
+++ src/com/example/javabutton/MainActivity.java
@@ -6,20 +6,40 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Resources;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
-import android.net.Uri;
+//import android.widget.Toast;
+import android.media.AudioManager;
+import android.media.SoundPool;
 
-public class MainActivity extends Activity implements SensorEventListener {
-    final Uri voiceuri= Uri.parse("android.resource://com.example.javabutton/raw/java22");
-    JavaPlayer jp=new JavaPlayer(voiceuri);
+public class MainActivity extends Activity implements SensorEventListener, GestureDetector.OnGestureListener {
     private SensorManager sensorManager;
     private Sensor accelerometer;
+    protected final float JAVA_MIN_ACCEL=600.f;
+    protected final float JAVA_MAX_ACCEL=1000.f;
+    private SoundPool javaPool;
+    private int maxJavaSounds=4;
+    private int javaSoundId;
+    private boolean evenShake=true;
+    private GestureDetector gDetector;
+	private float v_max;
+	private float v_min;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        javaPool=new SoundPool(maxJavaSounds,AudioManager.STREAM_MUSIC,0);
+        javaSoundId=javaPool.load(this, R.raw.java22, 1);
+        
+        Resources r=getResources();
+        v_max=r.getDimensionPixelSize(R.dimen.fling_velocity_max);
+        v_min=r.getDimensionPixelSize(R.dimen.fling_velocity_min);
+        
+        gDetector=new GestureDetector(this,this);
+        
         sensorManager= (SensorManager)getSystemService(SENSOR_SERVICE);
         accelerometer= sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
@@ -35,7 +55,12 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onPause();
         sensorManager.unregisterListener(this);
     }
-
+    
+    @Override
+    protected void onStop() {
+    	super.onStop();
+    	javaPool.release();
+    }
     /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,7 +71,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     */
     public void JavaButtonClick(View v) {
         //Toast.makeText(this,"Java",Toast.LENGTH_SHORT).show();
-    	jp.play(this);
+    	javaPool.play(javaSoundId, 1.0f, 1.0f, 1, 0, 1.0f);
     }
 
     @Override
@@ -57,10 +82,64 @@ public class MainActivity extends Activity implements SensorEventListener {
         z=event.values[2];
 
         float acceleration=x*x + y*y + z*z;
-        if (acceleration > 1000)
-            jp.play(this);
+        if (acceleration > JAVA_MIN_ACCEL) {
+        	if(evenShake) {
+        		acceleration=Math.min(acceleration, JAVA_MAX_ACCEL);
+            	float pitch=0.8f+1.2f*(acceleration-JAVA_MIN_ACCEL)/(JAVA_MAX_ACCEL-JAVA_MIN_ACCEL);
+            	javaPool.play(javaSoundId, 1.0f, 1.0f, 1, 0, pitch);
+            }
+        	evenShake=!evenShake;
+        }
+     }
+    
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return gDetector.onTouchEvent(event);
     }
-
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy){}
+
+	@Override
+	public boolean onDown(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean onFling(MotionEvent evt0, MotionEvent evt1,
+			float velocityX, float velocityY) {
+		float velocity=(float) Math.sqrt(velocityX*velocityX+velocityY*velocityY);
+		if(velocity>=v_min) {
+			float v2=Math.min(v_max, velocity);
+			float pitch=(v2-v_min)/(v_max-v_min)*1.3f+0.7f;
+			javaPool.play(javaSoundId, 1.0f, 1.0f, 1, 0, pitch);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void onLongPress(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent arg0, MotionEvent arg1, float arg2,
+			float arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
