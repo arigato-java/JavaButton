fe34883479d7dce954b049551698bf4638e853f7
Hisanobu Tomari <htomari@myopera.com>
2013-10-31 18:55:59 +0900

    Add back button on the preferences activity

--- src/com/example/javabutton/SettingsActivity.java
+++ src/com/example/javabutton/SettingsActivity.java
@@ -2,17 +2,27 @@ package com.example.javabutton;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.view.MenuItem;
 
 public class SettingsActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 		// Display the fragment as the main content.
 
 		getFragmentManager().beginTransaction()
 		.replace(android.R.id.content, new SettingsFragment())
 		.commit();
-    }
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int menuId=item.getItemId();
+		if(menuId==android.R.id.home) {
+			onBackPressed();
+			return true;
+		} else {
+			return super.onOptionsItemSelected(item);
+		}
+	}
 }
